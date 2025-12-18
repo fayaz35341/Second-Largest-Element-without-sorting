53d155eb6bbc49e2c7d844d7c7c43d56875a3a70
Riyaz Shaik <94755295+fayaz35341@users.noreply.github.com>
2025-12-18 21:12:41 +0530

    Create Second-Largest-Element.js

---
+++ Second-Largest-Element.js
@@ -0,0 +1,21 @@
+class Solution {
+    secondLargestElement(nums) {
+        let L=nums[0]
+        let SL=0 
+        for(let i=0; i<nums.length-1;i++){
+            if (nums[i]<nums[i+1]){
+                SL=L
+                L=nums[i+1]
+            }
+            if (L>nums[i]>SL){
+                SL=nums[i]
+            }
+        }
+        if (SL===0){
+            return '-1'
+        }
+        return SL
+    }
+}
+let n=[7, 7, 2, 2, 10, 10, 10]
+console.log(new Solution().secondLargestElement(n))
