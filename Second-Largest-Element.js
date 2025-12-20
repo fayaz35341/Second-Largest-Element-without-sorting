08e1c8cdecbfe1d0c0f11dbf9bcac62ed2b37ec4
Riyaz Shaik <94755295+fayaz35341@users.noreply.github.com>
2025-12-20 06:13:23 +0530

    Update Second-Largest-Element.js

--- Second-Largest-Element.js
+++ Second-Largest-Element.js
@@ -1,24 +1,42 @@
+// class Solution {
+//     secondLargestElement(nums) {
+//         let L=nums[0]
+//         let SL=0 
+//         for(let i=0; i<nums.length-1;i++){
+//             if (nums[i]<nums[i+1]){
+//                 SL=L
+//                 L=nums[i+1]
+//             }
+//             if (L>nums[i]>SL){
+//                 SL=nums[i]
+//             }
+//         }
+//         if (SL===0){
+//             return '-1'
+//         }
+//         else if (L>nums[i+1]>SL){ 
+//             SL=nums[i+1]
+//         }
+//         return SL
+//     }
+// }
 class Solution {
-    secondLargestElement(nums) {
-        let L=nums[0]
-        let SL=0 
-        for(let i=0; i<nums.length-1;i++){
-            if (nums[i]<nums[i+1]){
-                SL=L
-                L=nums[i+1]
+    getSecondLargest(arr) {
+        // code her
+        let L=-Infinity 
+        let SL=-Infinity  
+        for (let i=0; i<arr.length ; i++){
+            if (arr[i]> L){
+                SL=L 
+                L=arr[i]
+            }    
+            else if (L>arr[i]  && arr[i]>SL){ 
+                SL=arr[i]
             }
-            if (L>nums[i]>SL){
-                SL=nums[i]
-            }
         }
-        if (SL===0){
-            return '-1'
-        }
-        else if (L>nums[i+1]>SL){ 
-            SL=nums[i+1]
-        }
-        return SL
+        
+        return SL=== -Infinity ? "-1" : SL
     }
 }
 let n=[7, 7, 2, 2, 10, 10, 10]
-console.log(new Solution().secondLargestElement(n))
+console.log(new Solution().getSecondLargest(n))
